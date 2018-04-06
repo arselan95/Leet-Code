f4deb2d602497653ca0078279b62a5bd43d09a33
Arselan <arselan95@gmail.com>
2018-04-05 19:19:18 -0700

    Update Two_Sum.java

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,34 +1,35 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-        int a = 0,b = 0;
-    	int t = 0;
-        int[] arr=new int[2];
-     for(int i=0;i<nums.length;i++)
-     {
-    	 for(int j=1;j<nums.length;j++){
-    	  t=nums[i]+nums[j];
-    	  if(t==target&& i!=j)
-    	  {
-    		 a=j;
-    		 b=i;
-    	  }
-    	 }
-     }
-        
-        if(a>b)
-        {
-            arr[0]=b;
-            arr[1]=a;
-        }
-        else if(a<b)
-        {
-            arr[0]=a;
-            arr[1]=b;
-        }
-        return arr;
-        
-        
-        
+ class Solution {
+	
+	public static void twosum(int[] nums,int target) 
+    {
+		int[] res=new int[2];
+		HashMap<Integer,Integer>h1=new HashMap<Integer,Integer>();
+		for(int i=0;i<nums.length;i++)
+		{
+			int diff= target-nums[i];
+			if(h1.containsKey(diff))
+			{
+				res[0]=h1.get(diff);
+				res[1]=i;
+				for(int k:res){
+				System.out.println(k);
+				}
+			}
+			else{
+				h1.put(nums[i],i);
+			}
+		}	
+    	
     }
+    
+    public static void main(String[] args)
+    {
+    	int[] a={1,3,4,5};
+    	twosum(a,5);
+    	
+    	
+    			
+    
+    }
+
 }
