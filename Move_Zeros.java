93694b3f36b01cffef69294d5c1394b89c837a93
Arselan <arselan95@gmail.com>
2019-02-14 15:37:47 -0800

    Update Move_Zeros.java

--- Move_Zeros.java
+++ Move_Zeros.java
@@ -1,5 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        /*
+        Traverse the given array ‘arr’ 
+        from left to right. While traversing, maintain count of non-zero elements in array. 
+        Let the count be ‘count’. For every non-zero element arr[i], put the element at ‘arr[count]’ and increment ‘count’. 
+        After complete traversal, all non-zero elements have already been shifted to front end and ‘count’ is set as index of first 0. 
+        Now all we need to do is that run a loop which makes all elements zero from ‘count’ till end of the array.
+        O(n) where n is number of elements in input array.
+        */
         
  int count=0;
       for(int i =0;i<nums.length;i++)
