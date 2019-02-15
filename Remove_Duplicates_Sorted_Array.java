f8d1a25ab639eb2fddba826cfe04edc8b463abdd
Arselan <arselan95@gmail.com>
2019-02-14 19:31:12 -0800

    Update Remove_Duplicates_Sorted_Array.java

--- Remove_Duplicates_Sorted_Array.java
+++ Remove_Duplicates_Sorted_Array.java
@@ -1,4 +1,14 @@
 class Solution {
+    
+    /*
+    
+    Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+We check if consequtive values are equal as they are already sorted
+if not equal we add it to new array/ or dynamic array in this case
+    */
     public int removeDuplicates(int[] nums) {
         
         if(nums.length<2){
