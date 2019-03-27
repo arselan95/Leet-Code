f8f0b2e6e994404ecc399857bab653c0ebc6ee83
Arselan <arselan95@gmail.com>
2019-03-27 15:12:10 -0700

    Add files via upload

---
+++ designing/VendingMachine.java
@@ -0,0 +1,43 @@
+package designing;
+
+public class VendingMachine {
+	
+	/*
+	 * class VendingMachine
+	- boolean takeMoney(Money money);
+	
+	//Here item will contain type & quantity, no smart search required 
+	//say user need half litere Coke.
+	 * 
+	- Item pickItem(Item item); 
+	- boolean isAvilable(Item item)
+	- Double askForAmount(Input input);
+	- Double valiDateAndReturnBalance(Money money);
+class Money
+	-CurrencyType type;
+	-Double amount	
+class MoneyValidator
+    - boolean validate(Money amount);
+class Item
+	-ItemType type;
+	-Double quantity
+	-String barcode; // May be anything else to identify it
+class Input
+    - CommandType type;
+    - Double quantity
+	- ItemType type;
+class display
+    - void updateDisplay(String instructions)
+class KeyPad
+    - Input readUserInput(Key key);
+class Key
+    -KeyType type
+    -Double value
+	
+enum CurrencyType
+enum ItemType
+enum KeyType
+enum CommandType
+	 */
+
+}
