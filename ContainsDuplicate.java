a998bbe4a07c0fb75b9f0008e477abe7f876417c
Arselan <arselan95@gmail.com>
2019-04-07 14:06:11 -0700

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -13,14 +13,30 @@ Output: true
 public class ContainsDuplicate {
 	public boolean containsDuplicate(int[] nums) {
 
-		Arrays.sort(nums);
-
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (nums[i] == nums[i + 1])
-				return true;
-
-		}
-		return false;
+		 /*
+        
+        Arrays.sort(nums);
+        
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+                return true;
+           
+    }
+        return false;
+}
+*/
+        HashSet<Integer> set=new HashSet<Integer>();
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(set.contains(nums[i]))
+            {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;   
 	}
 
 }
