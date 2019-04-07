32ca1b82cc7011274b54dc4251ef73394555f796
Arselan <arselan95@gmail.com>
2019-04-07 14:48:21 -0700

    Update IntersectionOfTwoArrays_SET_INTERSECTION.java

--- IntersectionOfTwoArrays_SET_INTERSECTION.java
+++ IntersectionOfTwoArrays_SET_INTERSECTION.java
@@ -17,40 +17,56 @@ public class IntersectionOfTwoArrays_SET_INTERSECTION {
 
 	public int[] intersection(int[] nums1, int[] nums2) {
 
-		  List<Integer> set = new ArrayList<Integer>();
-		  
-		  //sort bthe arrays
-	        Arrays.sort(nums1);
-	        Arrays.sort(nums2);
-	        
-	        //if an element is common add that to the list else just move on
-	        int i = 0;
-	        int j = 0;
-	        while (i < nums1.length && j < nums2.length) {
-	            if (nums1[i] < nums2[j]) {
-	                i++;
-	            } else if (nums1[i] > nums2[j]) {
-	                j++;
-	            } else {
-	                if(set.contains(nums1[i]))
-	                {
-	                    i++;
-	                }
-	                else{
-	                set.add(nums1[i]);
-	                i++;
-	                j++;
-	                }
-	            }
-	        }
-	        //move elements of array bck to the list 
-	        int[] result = new int[set.size()];
-	        int k = 0;
-	        for (Integer num : set) {
-	            result[k++] = num;
-	        }
-	        return result;
-	        
+	  /*
+        List<Integer> set = new ArrayList<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0;
+        int j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                if(set.contains(nums1[i]))
+                {
+                    i++;
+                }
+                else{
+                set.add(nums1[i]);
+                i++;
+                j++;
+                }
+            }
+        }
+        int[] result = new int[set.size()];
+        int k = 0;
+        for (Integer num : set) {
+            result[k++] = num;
+        }
+        return result;
+        
+    }
+    */
+        
+      Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+		
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
 
 	}
 
